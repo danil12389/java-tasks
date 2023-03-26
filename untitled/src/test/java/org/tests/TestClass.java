d237527eef85a446c204c8a312dfebfdaec35bb9
danil12389 <akverrr@gmail.com>
2023-03-26 16:22:17 +0400

    сделал проверки атомарными

--- untitled/src/test/java/org/tests/TestClass.java
+++ untitled/src/test/java/org/tests/TestClass.java
@@ -5,23 +5,35 @@ import org.example.ToUpperCaseAnnProcessor;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestClass {
 
     Entity entity;
     ToUpperCaseAnnProcessor toUpperCaseAnnProcessor;
+
+    List<String> result;
+
     @Before
-    public void setUp() {
+    public void setUp() throws IllegalAccessException {
        entity = new Entity();
        toUpperCaseAnnProcessor = new ToUpperCaseAnnProcessor();
+       result = toUpperCaseAnnProcessor.process(entity);
     }
 
     @Test
-    public void checkAnnotationField() throws IllegalAccessException {
-        var result = toUpperCaseAnnProcessor.process(entity);
+    public void checkAmountAnnotationField() throws IllegalAccessException {
         assertEquals(2, result.size(), "Нам вернулись только аннотированные поля(2 из 4х)");
-        assertEquals("PUBLIC_ANNOTATED_FIELD", result.get(0), "Публичное аннотированное поле");
+    }
+
+    @Test
+    public void checkPublicAnnotationField() throws IllegalAccessException {
+        assertEquals("PUBLIC_ANNOTATED_FIELD", result.get(0), "Приватное аннотированное поле");
+    }
+    @Test
+    public void checkPrivateAnnotationField() throws IllegalAccessException {
         assertEquals("PRIVATE_ANNOTATED_FIELD", result.get(1), "Приватное аннотированное поле");
     }
 }
